4b0ecb5986bbb3b241f7fdf517a7c71f95faec84
Bruna Freitas Almeida <bubufreitas@hotmail.com>
2024-08-29 11:59:27 -0300

    criado metodos do CRUD

--- src/EasyControl.Api/Domain/Services/Classes/UsuarioService.cs
+++ src/EasyControl.Api/Domain/Services/Classes/UsuarioService.cs
@@ -1,4 +1,8 @@
+using System.Security.Cryptography;
+using System.Text;
+using AutoMapper;
 using EasyControl.Api.Contract.Usuario;
+using EasyControl.Api.Domain.Models;
 using EasyControl.Api.Domain.Repository.Interfaces;
 using EasyControl.Api.Domain.Services.Interfaces;
 
@@ -7,19 +11,11 @@ namespace EasyControl.Api.Domain.Services.Classes
     public class UsuarioService : IUsuarioService
     {
         private readonly IUsuarioRepository _usuarioRepository;
+        private readonly IMapper _mapper;
 
-        public UsuarioService(IUsuarioRepository usuarioRepository){
+        public UsuarioService(IUsuarioRepository usuarioRepository, IMapper mapper){
             _usuarioRepository = usuarioRepository;
-        }
-
-        public async Task<UsuarioLoginResponseContract> Adicionar(UsuarioRequestContract entidade, long idUsuario)
-        {
-            await _usuarioRepository.Adicionar();
-        }
-
-        public Task<UsuarioLoginResponseContract> Atualizar(long id, UsuarioRequestContract entidade, long idUsuario)
-        {
-            throw new NotImplementedException();
+            _mapper = mapper;
         }
 
         public Task<UsuarioLoginResponseContract> Autenticar(UsuarioLoginRequestContract usuarioLoginRequest)
@@ -27,19 +23,61 @@ namespace EasyControl.Api.Domain.Services.Classes
             throw new NotImplementedException();
         }
 
-        public Task Inativar(long id, long idUsuario)
+        public async Task<UsuarioResponseContract> Adicionar(UsuarioRequestContract entidade, long idUsuario)
         {
-            throw new NotImplementedException();
+            var usuario = _mapper.Map<Usuario>(entidade);
+
+            usuario.Senha = GerarHashSenha(usuario.Senha);
+            usuario = await _usuarioRepository.Adicionar(usuario);
+
+            return _mapper.Map<UsuarioResponseContract>(usuario);
         }
 
-        public Task<IEnumerable<UsuarioLoginResponseContract>> Obter(long idUsuario)
-        {
-            throw new NotImplementedException();
+        private string GerarHashSenha(string senha){
+            string hashSenha;
+
+            using(SHA256 sHA256 = SHA256.Create())
+            {
+                byte[] bytesSenha = Encoding.UTF8.GetBytes(senha);
+                byte[] bytesHashSenha = sHA256.ComputeHash(bytesSenha);
+                hashSenha = BitConverter.ToString(bytesHashSenha).ToLower();
+            }            
+            return hashSenha;
         }
 
-        public Task<UsuarioLoginResponseContract> Obter(long id, long idUsuario)
+        public async Task<UsuarioResponseContract> Atualizar(long id, UsuarioRequestContract entidade, long idUsuario)
         {
-            throw new NotImplementedException();
+            _ = await Obter(id) ?? throw new Exception("Usuário não encontrado para atualização!");
+
+            var usuario = _mapper.Map<Usuario>(entidade);
+            usuario.Id = id;
+            usuario.Senha = GerarHashSenha(entidade.Senha);
+            usuario = await _usuarioRepository.Atualizar(usuario);
+
+            return _mapper.Map<UsuarioResponseContract>(usuario);
+        }        
+
+        public async Task Inativar(long id, long idUsuario)
+        {
+            var usuario = await Obter(id) ?? throw new Exception("Usuário não encontrado para inativação!");
+            await _usuarioRepository.Deletar(_mapper.Map<Usuario>(usuario));
+        }
+
+        public async Task<IEnumerable<UsuarioResponseContract>> Obter(long idUsuario)
+        {
+            return await Obter(idUsuario);
+        }
+
+        public async Task<UsuarioResponseContract> Obter(long id, long idUsuario)
+        {
+            var usuario = await _usuarioRepository.Obter(id);
+            return _mapper.Map<UsuarioResponseContract>(usuario);
+        }
+        
+        public async Task<UsuarioResponseContract> Obter(string email)
+        {
+            var usuario = await _usuarioRepository.Obter(email);
+            return _mapper.Map<UsuarioResponseContract>(usuario);
         }
     }
 }
--- src/EasyControl.Api/obj/Debug/net8.0/EasyControl.Api.AssemblyInfo.cs
+++ src/EasyControl.Api/obj/Debug/net8.0/EasyControl.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasyControl.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c496d474d9fe4a302044d452cd71ce32b7335ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4a3c5d855a212b7d4e6da11081c3e3eff9cbe61")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasyControl.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasyControl.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
